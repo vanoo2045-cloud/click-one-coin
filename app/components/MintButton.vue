4cb0ed11da5d976f52c79ba4babb846058d975c3
Ваше Имя <@email.com>
2026-09-12 21:40:51 +0300

    paths of images

--- app/components/MintButton.vue
+++ app/components/MintButton.vue
@@ -59,7 +59,7 @@ function onClick() {
 <style scoped>
 
 .mint-button {
-  background-image: url("C:\NuxtGame\click-one\public\anvil3.png");
+  background-image: url("/click-one-coin/anvil3.png");
   background-size:contain;
   background-repeat: no-repeat;
   align-items: center;
